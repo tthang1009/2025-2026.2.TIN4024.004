434bd9a77e02ac066c877517d81a0699d0a736a0
LeeJMaiRap <22t1020071@husc.edu.vn>
2026-01-24 15:34:45 +0700

    commit led_blink

--- TEAM_04/NguyenThanhDoanh/LED_Blink/src/main.cpp
+++ TEAM_04/NguyenThanhDoanh/LED_Blink/src/main.cpp
@@ -1,27 +1,13 @@
-#include <Arduino.h>
-
-#define PIN_LED_RED 23
-
-//Non-blocking
-bool IsReady(unsigned long &ulTimer, uint32_t millisecond) {
-  if (millis() - ulTimer < millisecond) return false;
-  ulTimer = millis();
-  return true;
-}
-
+ #include <Arduino.h>
 void setup() {
-  // put your setup code here, to run once:
-  printf("WELCOME IOT\n");
-  pinMode(PIN_LED_RED, OUTPUT);
+  Serial.begin(115200);
+  pinMode(23, OUTPUT);
+  Serial.println("Hệ thống đã sẵn sàng!");
 }
 
-//Non-Blocking
 void loop() {
-  static unsigned long ulTimer = 0;
-  static bool lesStatus = false;
-  if (IsReady(ulTimer, 500)){
-    lesStatus = !lesStatus;
-    printf("LES IS [%s]\n",lesStatus ? "ON" : "OFF");
-    digitalWrite(PIN_LED_RED, lesStatus ? HIGH : LOW);
-  }
+  digitalWrite(23, HIGH);
+  delay(500);
+  digitalWrite(23, LOW);
+  delay(500);
 }
