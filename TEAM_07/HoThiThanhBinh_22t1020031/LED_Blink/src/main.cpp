d1d18cc3f59a276670482e19f42145e7ba055f03
22t1020031-droid <22t1020031@husc.edu.vn>
2026-01-24 18:09:31 +0700

    tt

---
+++ TEAM_07/HoThiThanhBinh_22t1020031/LED_Blink/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  printf("WELCOME IOT\n");
+
+}
+
+
+//BLOCKING
+void loop() {
+//   // put your main code here, to run repeatedly:
+//   // static int i = 0;
+//   // printf("Loop running ...%d\n",++i);
+//   // delay(1000);
+
+//   digitalWrite(PIN_LED_RED, HIGH); // Turn LED ON
+//   delay(500); // Wait for 500ms
+//   digitalWrite(PIN_LED_RED, LOW); // Turn LED OFF
+//   delay(500); // Wait for 500ms  
+ }
+
