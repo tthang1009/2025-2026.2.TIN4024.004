1b0534db87d01500472a76bb8e7d49a7d16a2c6c
NguyenHuuTringf <nguyenhuutrinh1009@gmail.com>
2026-01-27 13:06:21 +0700

    update

--- TEAM_06/NguyenHuuTrinh-22T1020481/Test1/src/main.cpp
+++ TEAM_06/NguyenHuuTrinh-22T1020481/Test1/src/main.cpp
@@ -1,15 +1,34 @@
 #include <Arduino.h>
+#define LED_RED    26
+#define LED_YELLOW 33
+#define LED_GREEN  32
 
-// put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  pinMode(LED_RED, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  delay(500);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, HIGH);
+  digitalWrite(LED_GREEN, LOW);
+  delay(500);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  delay(500);
 }
 
 // put function definitions here:
